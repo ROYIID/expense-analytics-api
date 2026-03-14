ee194d0148e58d9c6cd0d36d80bd99507c0eb193
Diyor Gaffarov <diyorgaffarov@gmail.com>
2026-03-14 21:43:59 +0500

    chore: preapre for shcema expences

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
+from app.api import expenses
 
 load_dotenv()
 
@@ -8,6 +9,8 @@ load_dotenv()
 app_name=os.environ.get("APP_NAME","Default Title")
 
 app = FastAPI(title=app_name, version="0.1.0")
+
+app.include_router(expenses.router)
 
 @app.get("/")
 def read_root():

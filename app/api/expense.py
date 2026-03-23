34dff367214ebc859db53e57705669cf53d50000
Diyor Gaffarov <diyorgaffarov@gmail.com>
2026-03-23 12:09:20 +0500

    feat: add async to functions and modify Http execptions

--- app/api/expense.py
+++ app/api/expense.py
@@ -7,7 +7,7 @@ router = APIRouter(prefix="/expenses", tags=["expenses"])
 expenses_db = []
 
 @router.post("/", status_code=201, response_model=ExpenseResponse)
-def create_expense(expense: ExpenseCreate):
+async def create_expense(expense: ExpenseCreate):
     expense_value = expense.model_dump()
     expense_value["id"]= len(expenses_db) + 1
     expenses_db.append(expense_value)
@@ -16,7 +16,13 @@ def create_expense(expense: ExpenseCreate):
 
 
 @router.get("/", response_model=list[ExpenseResponse])
-def get_expenses(category: str = None,min_amount: float = None,skip: int = 0, limit: int = 10):
+async def get_expenses(
+    category: str = None,
+    min_amount: float = None,
+    skip: int = 0,
+    limit: int = 10
+    ):
+    
     filtered_expenses = expenses_db
     if category:
         filtered_expenses = [expense for expense in filtered_expenses if expense["category"].lower() == category.lower()]
@@ -28,31 +34,31 @@ def get_expenses(category: str = None,min_amount: float = None,skip: int = 0, li
 
 
 @router.get("/{expense_id}", response_model=ExpenseResponse)
-def get_expense(expense_id: int):
+async def get_expense(expense_id: int):
     for expense in expenses_db:
         if expense["id"] == expense_id:
             return expense
-    raise HTTPException(status_code=404, detail="Expense not found")
+    raise HTTPException(status_code=404, detail=f"Expense {expense_id} not found")
 
 
 @router.put("/{expense_id}", response_model=ExpenseResponse)
-def update_expense(expense_id: int, expense_update: ExpenseUpdate):
+async def update_expense(expense_id: int, expense_update: ExpenseUpdate):
     for expense in expenses_db:
         if expense['id'] == expense_id:
             update_data =expense_update.model_dump(exclude_unset=True)
             expense.update(update_data)
             return expense
-    raise HTTPException(status_code=404, detail="Expense not found")
+    raise HTTPException(status_code=404, detail=f"Expense {expense_id} not found")
 
 
 
 @router.delete("/{expense_id}")
-def delete_expense(expense_id: int):
+async def delete_expense(expense_id: int):
     for  expense in expenses_db:
         if expense['id'] == expense_id:
             expenses_db.remove(expense)
             return {"detail": "Expense deleted"}
-    raise HTTPException(status_code=404, detail="Expense not found")
+    raise HTTPException(status_code=404, detail=f"Expense {expense_id} not found")
     
 
     
